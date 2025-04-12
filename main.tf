c59a1a3634f411c03e14e7438e103e463d02cc0e
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-12 02:47:18 -0400

    added database users

--- main.tf
+++ main.tf
@@ -44,3 +44,35 @@ resource "aws_iam_group_policy_attachment" "system_admin_policy" {
    group      = aws_iam_group.system_admins.name
    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+resource "aws_iam_user" "database_user1"{
+    name = "${var.d_u}_1"
+}
+variable "d_u" {
+    type = string
+    default = "database_admin_user"
+}
+resource "aws_iam_user" "database_user2"{
+    name = "${var.d_u}_2"
+}
+resource "aws_iam_user" "database_user3"{
+    name = "${var.d_u}_3"
+}
+resource "aws_iam_group" "database_admins" {
+   name = "database_admins"
+
+}
+resource "aws_iam_group_membership" "database" {
+   name = "database-admin-group-membership"
+
+   users = [
+     "${var.d_u}_1",
+     "${var.d_u}_2",
+     "${var.d_u}_3"
+   ]
+
+   group = aws_iam_group.database_admins.name
+}
+resource "aws_iam_group_policy_attachment" "database_admin_policy" {
+   group      = aws_iam_group.database_admins.name
+   policy_arn = "arn:aws:iam::aws:policy/job-function/DatabaseAdministrator"
+}
